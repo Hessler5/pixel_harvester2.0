6af34ef9794b669673ea58f44b973fcbc0fea898
Ethan Hessler <ethanhessler@gmail.com>
2024-02-07 13:48:36 -0500

    fixed tailwind

--- front_end/pixel_harvester/src/components/Header.jsx
+++ front_end/pixel_harvester/src/components/Header.jsx
@@ -1,8 +1,12 @@
-
+import React from "react";
 
 
 function Header() {
-    return "test"
+    return (
+        <div>
+            <img className = "w-36 h-36" src={"src/assets/Pixel_Harvester_Logo.webp"}/>
+        </div>
+    )
 }
 
 export default Header;
