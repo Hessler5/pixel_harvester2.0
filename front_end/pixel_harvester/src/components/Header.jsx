4d1013ab6a6944af45c328aa842a1e73b7109301
Ethan Hessler <ethanhessler@gmail.com>
2024-02-07 13:59:00 -0500

    finished header

--- front_end/pixel_harvester/src/components/Header.jsx
+++ front_end/pixel_harvester/src/components/Header.jsx
@@ -3,8 +3,17 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            <img className = "w-36 h-36" src={"src/assets/Pixel_Harvester_Logo.webp"}/>
+        <div className = "header flex justify-between">
+            <img className = "w-56 h-56 ml-2" src={"src/assets/Pixel_Harvester_Logo.webp"}/>
+            <div>
+                <h1 className = "text-6xl m-3 mt-6">User Name:</h1>
+                <h3 className = "text-3xl m-3">Remaining Scrapes:</h3>
+                <h3 className = "text-3xl m-3">Last Scrape:</h3>
+            </div>
+            <div className = 'flex items-end mr-2'>
+                <button className = 'm-3'>LOG OUT</button>
+                <button className = 'm-3'>ABOUT</button>
+            </div>
         </div>
     )
 }
